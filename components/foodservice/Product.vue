7e7e8e662e3c707a18e8aa2726e3ee63d23470eb
Gabriel Fauth <gabrielfauth@outlook.com>
2022-09-11 18:29:26 -0400

    adding qtd to product table

--- components/foodservice/Product.vue
+++ components/foodservice/Product.vue
@@ -20,6 +20,7 @@ export default {
                 { text: 'Produto', value: 'name' },
                 { text: 'Marca', value: 'brand' },
                 { text: 'Custo', value: 'cost' },
+                { text: 'Qtde.', value: 'stock' },
                 { text: 'Ações', value: 'actions' },
             ]
         }
